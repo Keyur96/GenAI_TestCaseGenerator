56574db6ddee3782be49ba8b5416ce4d4e1f504d
Keyur Modh <keyurmodh01@gmail.com>
2026-04-05 13:48:46 -0500

    Moved background images display code to top

--- app.py
+++ app.py
@@ -7,6 +7,25 @@ import os
 import re
 import io
 
+def add_bg_from_local(image_file):
+    with open(image_file, "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read())
+    st.markdown(
+    f"""
+    <style>
+    .stApp {{
+        background-image: url(data:image/{"png"};base64,{encoded_string.decode()});
+        background-size: cover
+    }}
+    header[data-testid="stHeader"] {{
+        background: transparent;
+    }}
+    </style>
+    """,
+    unsafe_allow_html=True
+    )
+add_bg_from_local('bg.png') 
+
 # Streamlit UI
 st.set_page_config(page_title="AI Test Case Generator", layout="wide")
 
@@ -180,21 +199,3 @@ if st.session_state.df is not None and not st.session_state.df.empty:
         mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
     )
     
-def add_bg_from_local(image_file):
-    with open(image_file, "rb") as image_file:
-        encoded_string = base64.b64encode(image_file.read())
-    st.markdown(
-    f"""
-    <style>
-    .stApp {{
-        background-image: url(data:image/{"png"};base64,{encoded_string.decode()});
-        background-size: cover
-    }}
-    header[data-testid="stHeader"] {{
-        background: transparent;
-    }}
-    </style>
-    """,
-    unsafe_allow_html=True
-    )
-add_bg_from_local('bg.png') 
